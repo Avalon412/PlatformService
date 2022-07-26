3e9f84ec23231f1a4b37ea871f40b53a8de77ace
Yevhen Vasylynchuk <marakasi88887@gmail.com>
2022-07-26 16:10:15 +0300

    Added synchronous messaging between services

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,9 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 
+Console.WriteLine($"--> CommandService Endpoint: {builder.Configuration["CommandService"]}");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
